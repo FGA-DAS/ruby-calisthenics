2099da8ce0dae0b772c7576946e9c3ee269001d8
Lucas Moura <lucas.moura128@gmail.com>
2015-08-28 17:19:07 -0300

    Word Count working

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -3,8 +3,18 @@ module FunWithStrings
         normalized = self.gsub(/[^A-Za-z0-9]/, '')
         return normalized.reverse.downcase == normalized.downcase
   end
+
   def count_words
-    # your code here
+      word_array = self.downcase.split(" ")
+      word_dict = Hash.new(0)
+
+      word_array.each do |word|
+          next unless word =~ /\w/
+          word = word.gsub(/[^A-Za-z0-9]/, '')
+          word_dict[word] = word_dict[word]+1
+      end
+
+      return word_dict
   end
   def anagram_groups
     # your code here
